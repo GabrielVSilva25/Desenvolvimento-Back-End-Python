095203a9a8f1b8a2cd417b3adbe6acefbc40cd07
Gabriel V Silva <47651757+GabrielVSilva25@users.noreply.github.com>
2026-07-23 20:48:18 -0300

    Fix note counting and average calculation logic

--- Estudos/Media_Notas.py
+++ Estudos/Media_Notas.py
@@ -1,21 +1,20 @@
-contador_notas = 1
+contador_notas = 0
 soma_notas = 0
-media = 0
+
+print('Digite -1 para encerrar o programa.')
 
 nota = float(input('Digite uma nota: '))
-print('Digite -1 para encerrar o programa.\n')
 
 while nota != -1:
     contador_notas +=1
     soma_notas += nota
 
     nota = float(input('Digite uma nota: '))
-    print(contador_notas)
 
-if nota == -1 and contador_notas == 1:
+if nota == -1 and contador_notas == 0:
     print('Programa encerrado.')
 
 else:
-    print(f'\nQuantidade de Notas: {contador_notas-1}')
+    print(f'\nQuantidade de Notas: {contador_notas}')
     print(f'Soma das notas: {soma_notas}')
-    print(f'Média das notas: {soma_notas/(contador_notas-1)}')
+    print(f'Média das notas: {soma_notas/(contador_notas)}')
